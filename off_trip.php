dc79f39ee0cb8b4780a868c6c81c18ce65a880e7
NejiYURI <halo010354@gmail.com>
2016-10-03 08:40:11 +0800

    增加取消旅遊
    更新sidebar.php與新增off_trip.php

---
+++ off_trip.php
@@ -0,0 +1,82 @@
+<? include("inc/header.php"); ?>
+    <? include("inc/navi.php"); ?>
+        <? include("inc/sidebar.php"); ?>
+            <!-- Page Content -->
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <? include ("inc/page-header.php"); ?>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div id="message">
+                                </div>
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        取消國民旅遊
+                                    </div>
+                                    <div class="panel-body">
+                                      <div class="table-responsive">
+                                        <table class="table table-striped table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>姓名</th>
+                                            <th>假別</th>
+                                            <th>起始日期</th>
+                                            <th>中止日期</th>
+                                            <th>起始時間</th>
+                                            <th>中止時間</th>
+                                            <th>總時間</th>
+                                            <th>職務代理人</th>
+                                            <th>不刷旅遊卡了</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                          <th>李_朗</th>
+                                          <th>休假(教職員)</th>
+                                          <th>0990113</th>
+                                          <th>0990113</th>
+                                          <th>8</th>
+                                          <th>17</th>
+                                          <th>1天0時</th>
+                                          <th>傘店</th>
+                                          <th><button type="button" class="btn btn-default">取消</button></th>
+                                        </tr>
+                                        <tr>
+                                          <th>PPAP</th>
+                                          <th>休假(教職員)</th>
+                                          <th>0990112</th>
+                                          <th>0990112</th>
+                                          <th>8</th>
+                                          <th>17</th>
+                                          <th>1天0時</th>
+                                          <th>傘店</th>
+                                          <th><button type="button" class="btn btn-default">取消</button></th>
+                                        </tr>
+                                        <tr>
+                                          <th>李_朗</th>
+                                          <th>休假(教職員)</th>
+                                          <th>0990112</th>
+                                          <th>0990112</th>
+                                          <th>8</th>
+                                          <th>17</th>
+                                          <th>1天0時</th>
+                                          <th>傘店</th>
+                                          <th><button type="button" class="btn btn-default">取消</button></th>
+                                        </tr>
+                                    </tbody>
+                                  </table>
+                                      </div>
+
+                                    </div>
+                                    <!--<div class="panel-footer">
+                                    </div>-->
+                                </div>
+                            </div>
+                            <!-- /.col-lg-12 -->
+                        </div>
+                        <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+            <? include("inc/footer.php"); ?>
