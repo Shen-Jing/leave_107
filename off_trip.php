39d5dcd28fc91a9670cd630bbdfc499db5eec0b4
NejiYURI <halo010354@gmail.com>
2016-10-17 00:36:00 +0800

    更新OFF_TRIP

--- off_trip.php
+++ off_trip.php
@@ -1,3 +1,8 @@
+<?php
+   session_start();
+   $conn=OCILOGON("exampg","exampg_vm","//120.107.186.109/project") or die("failed");
+     $userid =$_SESSION["empl_no"];
+?>
 <? include("inc/header.php"); ?>
     <? include("inc/navi.php"); ?>
         <? include("inc/sidebar.php"); ?>
@@ -13,6 +18,7 @@
                                     <div class="panel-heading">
                                         取消國民旅遊
                                     </div>
+
                                     <div class="panel-body">
                                       <div class="table-responsive">
                                         <table class="table table-striped table-bordered table-hover">
@@ -30,41 +36,92 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                          <th>李_朗</th>
-                                          <th>休假(教職員)</th>
-                                          <th>0990113</th>
-                                          <th>0990113</th>
-                                          <th>8</th>
-                                          <th>17</th>
-                                          <th>1天0時</th>
-                                          <th>傘店</th>
-                                          <th><button type="button" class="btn btn-default">取消</button></th>
-                                        </tr>
-                                        <tr>
-                                          <th>PPAP</th>
-                                          <th>休假(教職員)</th>
-                                          <th>0990112</th>
-                                          <th>0990112</th>
-                                          <th>8</th>
-                                          <th>17</th>
-                                          <th>1天0時</th>
-                                          <th>傘店</th>
-                                          <th><button type="button" class="btn btn-default">取消</button></th>
-                                        </tr>
-                                        <tr>
-                                          <th>李_朗</th>
-                                          <th>休假(教職員)</th>
-                                          <th>0990112</th>
-                                          <th>0990112</th>
-                                          <th>8</th>
-                                          <th>17</th>
-                                          <th>1天0時</th>
-                                          <th>傘店</th>
-                                          <th><button type="button" class="btn btn-default">取消</button></th>
-                                        </tr>
+                                      <?php
+
+                                      $sql = "SELECT count(*) count
+                                    				FROM holidayform
+                                    				where POCARD='$userid'
+                                    				and  trip=2
+                                    				and condition <> -1";
+                                    	$stmt=ociparse($conn,$sql);
+                                    	ociexecute($stmt,OCI_DEFAULT);
+                                    	if (OCIFETCH($stmt))
+                                    	   $count=ociresult($stmt,"COUNT");
+                                     if ($count==0)
+                                      	echo "";
+                                     else{
+                                    	$SQLStr ="SELECT empl_chn_name,h.POCARD,pc.CODE_CHN_ITEM,h.POVDATEB,h.POVDATEE,
+                                    	h.POVHOURS,h.POVTIMEB,h.POVTIMEE,h.POVDAYS,h.ABROAD,h.AGENTNO,
+                                        h.serialno,h.CURENTSTATUS
+                                    	FROM psfempl p,holidayform h,psqcode pc
+                                    	where h.POCARD='$userid'
+                                    	and condition <> -1
+                                    	and  trip=2
+                                    	and  p.empl_no=h.pocard
+                                    	and  pc.CODE_KIND='0302'
+                                    	and  pc.CODE_FIELD=h.POVTYPE
+                                    	order by h.POCARD,h.POVDATEB,h.POVHOURS";
+
+                                         $res = OCIParse($conn,$SQLStr);//liru update
+                                         OCIExecute($res);
+
+                                      while (OCIFetchInto($res, $row, OCI_ASSOC)){//liru update
+
+                                    		$poname = $row['EMPL_CHN_NAME'];
+                                    		$pocard = $row['POCARD'];
+                                    		$povtype= $row['CODE_CHN_ITEM'];
+                                    		$povdateB = $row['POVDATEB'];
+                                    		$povdatee = $row['POVDATEE'];
+                                    		$povhours = $row['POVHOURS'];
+                                    		$povdays  = $row['POVDAYS'];
+                                    		$povtimeb = $row['POVTIMEB'];  //liru add
+                                    		$povtimee  = $row['POVTIMEE'];  //liru add
+                                    		$abroad  = $row['ABROAD'];
+                                    		$agentno  = $row['AGENTNO'];
+                                    		$serialno = $row['SERIALNO'];
+
+                                    		$SQLStr2 = "SELECT EMPL_CHN_NAME FROM PSFEMPL where EMPL_NO='$agentno' ";
+                                    		$stmt=ociparse($conn,$SQLStr2);
+                                    		ociexecute($stmt,OCI_DEFAULT);
+                                    		$agentname='';
+                                    		if (OCIFETCH($stmt))
+                                    		   $agentname=ociresult($stmt,"EMPL_CHN_NAME");
+
+                                    		//...........................................................
+                                    		//10201 add
+                                    				if (strlen($povtimeb) > 2)
+                                    					$povtimeb=substr($povtimeb,0,2).":".substr($povtimeb,2,2);
+
+                                    				if (strlen($povtimee) > 2)
+                                    					$povtimee=substr($povtimee,0,2).":".substr($povtimee,2,2);
+                                    		//...........................................................
+
+                                    		echo "<tr><th>" ;
+                                    		echo $poname ;
+                                    		echo "</th><th>" ;
+                                    		echo $povtype ;
+                                    		echo "</th><th>" ;
+                                    		echo $povdateB ;
+                                    		echo "</th><th>" ;
+                                    		echo $povdatee ;
+                                    		echo "</th><th>" ;
+                                    		echo $povtimeb ;
+                                    		echo "</th><th>" ;
+                                    		echo $povtimee ;
+                                    		echo "</th><th>" ;
+                                    		echo $povdays."天".$povhours."時" ;
+                                    		echo "</th><th>" ;
+                                    		echo $agentname;
+                                    		echo "</th><th>" ;
+                                    		echo "<button type=\"button\" class=\"btn btn-default\">取消</button>" ;
+                                    		echo "</th></tr>";
+                                    	}
+                                      ?>
+
+
                                     </tbody>
                                   </table>
+                                <? } ?>
                                       </div>
 
                                     </div>
