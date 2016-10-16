9cf1693cf994895771225ac400d05a10d264079b
ShenJing <ShenJingnerve@gmail.com>
2016-10-17 01:10:16 +0800

    修正login_ajax.php中sql判斷帳號相等的部分

--- ajax/login_ajax.php
+++ ajax/login_ajax.php
@@ -22,12 +22,12 @@
 				  WHERE empl_no = crjb_empl_no
 				  AND crjb_depart = dept_no
 				  AND crjb_seq = '1'
-				  AND substr(empl_no,1,1) !='A'
+				  AND substr(empl_no, 1, 1) !='A'
 				  AND crjb_quit_date IS NULL
-				  AND psfempl.email = '" . $_POST['userid'] . "'";
+				  AND psfempl.email = '" . $user . "@cc.ncue.edu.tw'";
         $data = $db -> query_array($sql);
 
-				// 若有符合的則給定各項$_SESSION值
+				// 若有符合則給定各項$_SESSION值
         if (count($data['DEPT_FULL_NAME']) > 0) {
 						$_SESSION['_ID'] = $_POST['userid'];
 						$_SESSION['dept_name'] = $data['DEPT_FULL_NAME'];
