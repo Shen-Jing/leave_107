81ba481b5d7b5529a69910e0e5d80d2530282d4d
katoreto <uzukiengel@gmail.com>
2016-10-24 01:42:43 +0800

    新增加班紀錄查詢頁面(overtime_query.php)

---
+++ overtime_query.php
@@ -0,0 +1,53 @@
+<? include("inc/check.php");?>
+<? include("inc/header.php"); ?>
+    <? include("inc/navi.php"); ?>
+        <? include("inc/sidebar.php"); ?>
+
+            <!-- Page Content -->
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <? include ("inc/page-header.php"); ?>
+                        <form class="form-horizontal" role="form" name="form1" id="form1" action="" method="post" target="right">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div id="message">
+                                </div>
+                                <div class="panel panel-success">
+                                    <div class="panel-heading">
+                                        <font STYLE="font-family:微軟正黑體">請選擇查詢年度</font>
+                                    </div>
+                                    <div class="panel-body">
+                                        <select class="form-control" name="p_menu" id="qry_year" data-width="auto"></select>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.col-lg-12 -->
+                        </div>
+                        <!-- /
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        本年度加班記錄
+                                    </div>
+                                    <div class="panel-body ">
+                                        
+                                            <!--<fieldset>-->
+                                                <div class="form-group">
+                                                    <div>     
+                                                    </div>
+                                                    <!--link rel="stylesheet" href="style.css"-->
+                                                    <div id="_content">
+                                                    </div>
+                                                </div>
+                                            <!--</fieldset>-->
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wra
+            <? include("inc/footer.php"); ?>    
