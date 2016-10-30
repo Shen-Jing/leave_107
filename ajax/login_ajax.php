cd26ed1065c99830ae56049d4f8f02be43266cf1
ShenJing <ShenJingnerve@gmail.com>
2016-10-31 00:37:14 +0800

    初始登入時的員工各種SESSION資料，補上範例註解，好讓其他維護人員清楚該項目為何，省去重複撰寫取值的可能

--- ajax/login_ajax.php
+++ ajax/login_ajax.php
@@ -18,24 +18,29 @@
    if ($pwd == "test123" )  {
 		 		// 根據輸入的帳號查找此人
 				$sql = "SELECT dept_full_name, empl_chn_name, empl_no, CRJB_DEPART, CRJB_TITLE
-		          FROM  psfempl, psfcrjb, stfdept
-				  WHERE empl_no = crjb_empl_no
-				  AND crjb_depart = dept_no
-				  AND crjb_seq = '1'
-				  AND substr(empl_no, 1, 1) !='A'
-				  AND crjb_quit_date IS NULL
-				  AND psfempl.email = '" . $user . "@cc.ncue.edu.tw'";
+					      FROM  psfempl, psfcrjb, stfdept
+							  WHERE empl_no = crjb_empl_no
+							  AND crjb_depart = dept_no
+							  AND crjb_seq = '1'
+							  AND substr(empl_no, 1, 1) !='A'
+							  AND crjb_quit_date IS NULL
+							  AND psfempl.email = '" . $user . "@cc.ncue.edu.tw'";
         $data = $db -> query_array($sql);
 
 				// 若有符合則給定各項$_SESSION值
         if (count($data['DEPT_FULL_NAME']) > 0) {
+            // 0ob
 						$_SESSION['_ID'] = $_POST['userid'];
+            // 圖書與資訊處系統開發組
 						$_SESSION['dept_name'] = $data['DEPT_FULL_NAME'];
+            // 李_朗
 						$_SESSION['empl_name'] = $data['EMPL_CHN_NAME'];
+            // 0000676
 						$_SESSION['empl_no'] = $data['EMPL_NO'];
+            // MQ5
 						$_SESSION['depart'] = $data['CRJB_DEPART'];
-						$_SESSION['class_depart'] = $data['CRJB_DEPART'];
-						$_SESSION['title'] = $data['CRJB_TITLE'];
+            // F50
+						$_SESSION['title_id'] = $data['CRJB_TITLE'];
 						$_SESSION['logout'] = 0;
 
             $message = array("error_code"=>0,"error_message"=>0,"sql"=>"");
