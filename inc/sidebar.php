4d0dfcaf4376f65900a1dac0904a7f9724114747
katoreto <uzukiengel@gmail.com>
2016-11-20 21:27:34 +0800

    修正版本問題

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -10,77 +10,6 @@ $d = $db -> query_array($sql);
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-<<<<<<< HEAD
-                      <li>
-                        <a href='#'><i class='fa fa-edit fa-fw'></i><span class='masked'>簽核<span class='fa arrow'></span></span></a>
-                      </li>
-                      <li>
-                        <a href='#'><i class='fa fa-suitcase fa-fw'></i><span class='masked'>請假管理<span class='fa arrow'></span></span></a>
-                        <ul class="nav nav-second-level">
-                          <li>
-                            <a href='holiday_form.php'><span class='masked'>行政人員差假</span></a>
-                          </li>
-                          <li>
-                            <a href='#'><span class='masked'>單位差假狀況</span></a>
-                          </li>
-                          <li>
-                            <a href='update.php'><span class='masked'>修改假單</span></a>
-                          </li>
-                          <li>
-                            <a href='off_trip.php'><span class='masked'>取消國民旅遊</span></a>
-                          </li>
-                          <li>
-                            <a href='holiday_view.php'><span class='masked'>個人差假狀況(上)</span></a>
-                          </li>
-                          <li>
-                            <a href='#'><span class='masked'>個人差假統計資料</span></a>
-                          </li>
-                          <li>
-                            <a href='search_detail.php'><span class='masked'>個人差假明細資料</span></a>
-                          </li>
-                          <li>
-                            <a href='search_card.php'><span class='masked'>上下班刷卡資料</span></a>
-                          </li>
-                          <li>
-                            <a href='overtime.php'><span class='masked'>加班申請作業</span></a>
-                          </li>
-                          <li>
-                            <a href='overtime_query.php'><span class='masked'>加班記錄查詢</span></a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href='#'><i class='fa fa-car fa-fw'></i><span class='masked'>差勤管理<span class='fa arrow'></span></span></a>
-                        <ul class="nav nav-second-level">
-                          <li>
-                            <a href='#'><span class='masked'>國民旅遊名單</span></a>
-                          </li>
-                          <li>
-                            <a href='overtime_check.php'><span class='masked'>加班審核</span></a>
-                          </li>
-                          <li>
-                            <a href='#'><span class='masked'>加班已審核</span></a>
-                          </li>
-                          <li>
-                            <a href='p_search_overtime_idx.php'><span class='masked'>加班資料修改</span></a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href='#'><i class='fa fa-building fa-fw'></i><span class='masked'>制度管理<span class='fa arrow'></span></span></a>
-                        <ul class="nav nav-second-level">
-                          <li>
-                            <a href='#'><span class='masked'>有課老師請假名單</span></a>
-                          </li>
-                          <li>
-                            <a href='#'><span class='masked'>承辦員調補課簽核</span></a>
-                          </li>
-                          <li>
-                            <a href='#'><span class='masked'>所有調補課簽核狀況</span></a>
-                          </li>
-                        </ul>
-                      </li>
-=======
                       <?
                         $parent_previous = "";
                         for ($i=0; $i < sizeof($d['PGMNAME']); $i++) {
@@ -124,7 +53,6 @@ $d = $db -> query_array($sql);
                               echo "</ul></li>\r\n";
                         }
                       ?>
->>>>>>> 8d59b7651843cfbcf88a3ce5fb7a7b8962e82dfd
                     </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
