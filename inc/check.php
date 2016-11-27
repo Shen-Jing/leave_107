6b4e80ca6fc898f5ee1a6472e500051e768b1759
ShenJing <ShenJingnerve@gmail.com>
2016-11-28 01:18:05 +0800

    新增各頁面顯示「該頁面名稱」功能

--- inc/check.php
+++ inc/check.php
@@ -12,12 +12,15 @@
       }
 
       //檢查授權
-      // $now = basename($_SERVER['REQUEST_URI']); //aaa.bbb.ccc/xxx ==> xxx
-      // //$now = str_replace($base_url , "", $_SERVER['REQUEST_URI']);
-      // $sql = "select *
-      //             from sysgrpuser a ,sysgrppgm b ,syspgm c
-      //             where a.userid='".$_SESSION['_ID'] . "' and a.grpid = b.grpid and b.pgmid=c.pgmid  and c.pgmurl like '%$now' ";
-      //
+      $now = basename($_SERVER['REQUEST_URI']); //aaa.bbb.ccc/xxx ==> xxx
+      // $now = str_replace($base_url , "", $_SERVER['REQUEST_URI']);
+      $sql ="SELECT DISTINCT pgmname,pgmurl
+          FROM syspgm
+          WHERE sysid='LEAVE'
+          AND syspgm.pgmurl LIKE '%$now'";
+      $d = $db -> query_array($sql);
+      $_SESSION["pgmname"] = $d['PGMNAME'][0];
+
       // $d = $db -> query_array($sql);
       // if(sizeof($d['USERID'])==0 && $now !="index.php"){
       //     header('Content-Type: text/html; charset=utf-8');
@@ -25,7 +28,6 @@
       //     echo "<script>alert('您未被授權!');top.location.href='$url';</script>";
       //     exit;
       // }
-      // $_SESSION["pgmname"] = $d['PGMNAME'][0];
       //
       // //檢查招生類別
       // if( (strlen($_SESSION['school_id'])!=1  || strlen($_SESSION['year'])!=3 ) && $now !="index.php"){
