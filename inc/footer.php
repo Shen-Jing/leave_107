cc5fa651dafd6b14b5ba4d684e059d9f16636aca
ringley <rayf83186@gmail.com>
2016-12-20 13:55:08 +0800

    隱藏找不到頁面編輯器元件時的warning

--- inc/footer.php
+++ inc/footer.php
@@ -37,7 +37,7 @@
     <script src="js/jquery.jqGrid.min.js" type="text/ecmascript"></script>
     <script src="js/bootbox.min.js"></script>
     <script src="js/jquery-sortable-min.js"></script>
-	<? include_once( (dirname(__DIR__)) . "/page_editor/components/new_thread_js.php" ); ?>
+	<? @include_once( (dirname(__DIR__)) . "/page_editor/components/new_thread_js.php" ); ?>
     <script src="js/<?=basename($_SERVER['PHP_SELF'], ".php")?>.js<?='?'.time()?>"></script> <!-- Ensure reloading the newest javascript every time -->
 
 </body>
--- inc/header.php
+++ inc/header.php
@@ -58,7 +58,7 @@ $now = basename($_SERVER['REQUEST_URI']);
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-	<? include_once( (dirname(__DIR__)) . "/page_editor/components/new_thread_css.php" ); ?>
+	<? @include_once( (dirname(__DIR__)) . "/page_editor/components/new_thread_css.php" ); ?>
 
 </head>
 
