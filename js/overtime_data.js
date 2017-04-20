331f71ef31952d9d42f703afbe37014022ab410f
ShenJing <ShenJingnerve@gmail.com>
2017-04-20 22:17:22 +0800

    修改：「加班資料修改」，呼叫overtime_data.php填充modal，兩頁面的js中的CRUD會呼叫上的衝突，導致叫資料錯誤，已將後者改名。 另外修改顯示資料上的小瑕疵（「功能」的<th>底下硬是多一格，無解中，故只好將colspan縮減一格）

--- js/overtime_data.js
+++ js/overtime_data.js
@@ -28,14 +28,14 @@ $( // 表示網頁完成後才會載入
         $('#qry_year').change( // 抓取年份資料
             function(e) {
                 if ($(':selected', this).val() !== '') {
-                    CRUD(0); // 查詢加班記錄
+                    CRUD_overtime(0); // 查詢加班記錄
                 }
             }
         );
     }
 );
 
-function CRUD(oper, over_date) {
+function CRUD_overtime(oper, over_date) {
     over_date = over_date || ''; // 預設值
     // if (oper == 3){
     //   if (!confirm("是否確定要刪除?"))
@@ -57,40 +57,42 @@ function CRUD(oper, over_date) {
         dataType: "json",
         success: function(JData) {
             if (JData.error_code)
-                //toastr["error"](JData.error_message);
                 message(JData.error_message, "danger", 5000);
             else {
                 if (oper == "0") { //查詢
                     $('#_overtime-data').empty();
                     if (JData.EMPL_NO.length == 0) {
-                        $('#_overtime-data').append("<tr><td colspan='6'>目前尚無加班資料<td></tr>");
+                      // 此處不懂為何明明<th>有6格，colspan設定6會導致多凸出「功能」的那一塊
+                        $('#_overtime-data').append("<tr><td colspan='5'>目前尚無加班資料<td></tr>");
                     }
-                    for (var i = 0; i < JData.EMPL_NO.length; i++) {
-                        var row = "<tr>";
-                        // 加班日期
-                        row = row + "<td>" + JData.OVER_DATE[i] + "</td>";
-                        // 加班起始時間
-                        row = row + "<td>" + JData.DO_TIME_1[i] + "</td>";
-                        // 加班結束時間
-                        row = row + "<td>" + JData.DO_TIME_2[i] + "</td>";
-                        // 目前剩餘時數（input: 可修改）
-                        row = row + "<td><input value='" + JData.NOUSE_TIME[i] + "' type='number' min='0' max='99' id='nouse_time" + JData.OVER_DATE[i] + "' class='form-control'></td>";
-                        // 到期日期（input: 可修改）
-                        row = row + "<td><input value='" + JData.DUE_DATE[i] + "' type='text' id='due_date" + JData.OVER_DATE[i] + "' class='form-control'></td>";
-                        // 修改儲存按鈕
-                        row = row + "<td><button type='button' class='btn-success' name='modify' id='modify' title='修改儲存' onclick='CRUD(2, " + JData.OVER_DATE[i] + ")'><i class='fa fa-save'></i> </button></td>";
-                        row = row + "</tr>";
-                        $('#_overtime-data').append(row);
+                    else {
+                        for (var i = 0; i < JData.EMPL_NO.length; i++) {
+                            var row = "<tr>";
+                            // 加班日期
+                            row = row + "<td>" + JData.OVER_DATE[i] + "</td>";
+                            // 加班起始時間
+                            row = row + "<td>" + JData.DO_TIME_1[i] + "</td>";
+                            // 加班結束時間
+                            row = row + "<td>" + JData.DO_TIME_2[i] + "</td>";
+                            // 目前剩餘時數（input: 可修改）
+                            row = row + "<td><input value='" + JData.NOUSE_TIME[i] + "' type='number' min='0' max='99' id='nouse_time" + JData.OVER_DATE[i] + "' class='form-control'></td>";
+                            // 到期日期（input: 可修改）
+                            row = row + "<td><input value='" + JData.DUE_DATE[i] + "' type='text' id='due_date" + JData.OVER_DATE[i] + "' class='form-control'></td>";
+                            // 修改儲存按鈕
+                            row = row + "<td><button type='button' class='btn-success' name='modify' id='modify' title='修改儲存' onclick='CRUD_overtime(2, " + JData.OVER_DATE[i] + ")'><i class='fa fa-save'></i> </button></td>";
+                            row = row + "</tr>";
+                            $('#_overtime-data').append(row);
+                        }
                     }
                 } else if (oper == 1) { //新增
                     toastr["success"]("資料新增成功!");
-                    CRUD(0); //reload
+                    CRUD_overtime(0); //reload
                 } else if (oper == 2) { //修改
                     toastr["success"]("資料修改成功!");
-                    CRUD(0); //reload
+                    CRUD_overtime(0); //reload
                 } else if (oper == 3) { //刪除
                     toastr["success"]("資料刪除成功!");
-                    CRUD(0); //reload
+                    CRUD_overtime(0); //reload
                 }
             }
         },
@@ -101,7 +103,7 @@ function CRUD(oper, over_date) {
             $('#modal-loading').hide();
         },
         error: function(xhr, ajaxOptions, thrownError) {
-            console.log(xhr.responseText);
+            //console.log(xhr.responseText);
         }
     });
 }
