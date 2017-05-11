9fe44866621ecab6368c9ccf4f10717c579c758c
ringley <rayf83186@gmail.com>
2017-05-11 15:20:54 +0800

    修改假單：調整panel顯示

--- js/update.js
+++ js/update.js
@@ -1,121 +1,127 @@
 $(function() {
-    $("body").tooltip({
-        selector: "[title]"
-    });
-
     init_table();
-	
-	bootbox.setDefaults({
-		locale: "zh_TW",
-		backdrop: true
-	});
+
+    bootbox.setDefaults({
+        locale: "zh_TW",
+        backdrop: true
+    });
 });
 
 var utb, ctb;
+
 function init_table() {
-	utb = 
-    $('#update_table').DataTable({
-		responsive: true,
-        columns: [
-            { "data": "EMPL_CHN_NAME" },
-            { "data": "CODE_CHN_ITEM" },
-            { "data": "POVDATEB" },
-            { "data": "POVDATEE" },
-            { "data": "POVTIMEB" },
-            { "data": "POVTIMEE" },
-            { "data": "AGGRETIME" },
-            { "data": "AGENTNAME" },
-            { "data": "SERIALNO" }
-        ],
-        columnDefs: [{
-			"targets": 8,
-			"data": "SERIALNO",
-			"orderable": false,
-			"render": function(data) { 
-						// 修改
-				return  "<button type='button' class='btn btn-warning'" +
-						" onclick='openHoildayForm(" + data + ")' >" +
-						"<i class='fa fa-pencil'></i>" +
-						"</button>" + "&nbsp;" +
-						// 取消
-						"<button type='button' class='btn btn-danger'" +
-						" onclick='cancelHoildayForm(0," + data + ")' >" +
-						"<i class='fa fa-times' aria-hidden='true'></i>" +
-						"</button>";
-				}
+    utb =
+        $('#update_table').DataTable({
+            // 無資料時收起
+            fnInitComplete: function() {
+                if ($(this).find('tbody tr').length <= 1) {
+                    togglePanel($(this).parent().parent());
+                }
             },
-            { "className": "dt-center", "targets": "_all" }
-        ],
-        ajax: {
-            url: 'ajax/update_ajax.php',
-            type: 'POST',
-            dataType: 'json',
-			data: {oper: "qry"}
+            responsive: true,
+            columns: [
+                { "data": "EMPL_CHN_NAME" },
+                { "data": "CODE_CHN_ITEM" },
+                { "data": "POVDATEB" },
+                { "data": "POVDATEE" },
+                { "data": "POVTIMEB" },
+                { "data": "POVTIMEE" },
+                { "data": "AGGRETIME" },
+                { "data": "AGENTNAME" },
+                { "data": "SERIALNO" }
+            ],
+            columnDefs: [{
+                    "targets": 8,
+                    "data": "SERIALNO",
+                    "orderable": false,
+                    "render": function(data) {
+                        // 修改
+                        return "<button type='button' class='btn btn-warning'" +
+                            " onclick='openHoildayForm(" + data + ")' >" +
+                            "<i class='fa fa-pencil'></i>" +
+                            "</button>" + "&nbsp;" +
+                            // 取消
+                            "<button type='button' class='btn btn-danger'" +
+                            " onclick='cancelHoildayForm(0," + data + ")' >" +
+                            "<i class='fa fa-times' aria-hidden='true'></i>" +
+                            "</button>";
+                    }
+                },
+                { "className": "dt-center", "targets": "_all" }
+            ],
+            ajax: {
+                url: 'ajax/update_ajax.php',
+                type: 'POST',
+                dataType: 'json',
+                data: { oper: "qry" }
+            },
+            dom: 'tp'
+        });
+
+    ctb =
+        $('#cancel_table').DataTable({
+            responsive: true,
+            columns: [
+                { data: "EMPL_CHN_NAME" },
+                { data: "CODE_CHN_ITEM" },
+                { data: "POVDATEB" },
+                { data: "POVDATEE" },
+                { data: "POVTIMEB" },
+                { data: "POVTIMEE" },
+                { data: "AGGRETIME" },
+                { data: "AGENTNAME" },
+                { data: "SERIALNO" }
+            ],
+            columnDefs: [{
+                    "targets": 8,
+                    "data": "SERIALNO",
+                    "orderable": false,
+                    "render": function(data) {
+                        // 取消
+                        return "<button type='button' class='btn btn-danger' onclick='cancelHoildayForm(1," + data + ")'><i class='fa fa-times' aria-hidden='true'></i></button>";
+                    }
+                },
+                { "className": "dt-center", "targets": "_all" }
+            ],
+            ajax: {
+                url: 'ajax/update_ajax.php',
+                type: 'POST',
+                dataType: 'json',
+                data: { oper: "qry2" }
+            }
+        });
+}
+
+function openHoildayForm(sn) {
+    if (typeof sn == 'undefined')
+        return;
+
+    var url = "update_form.php?sn=" + sn;
+
+    $('.NewPage-IFrame').attr("src", url);
+    $('#fullscrModal').modal('show');
+}
+
+function cancelHoildayForm(flag, sn) {
+    bootbox.confirm({
+        message: "確定要取消該假單嗎？",
+        buttons: {
+            confirm: {
+                className: 'btn-danger'
+            }
         },
-        dom: 'tp'
-    });
-	
-	ctb =
-	$('#cancel_table').DataTable({
-		responsive: true,
-        columns: [
-            { data: "EMPL_CHN_NAME" },
-            { data: "CODE_CHN_ITEM" },
-            { data: "POVDATEB" },
-            { data: "POVDATEE" },
-            { data: "POVTIMEB" },
-            { data: "POVTIMEE" },
-            { data: "AGGRETIME" },
-            { data: "AGENTNAME" },
-            { data: "SERIALNO" }
-        ],
-        columnDefs: [{
-			"targets": 8,
-			"data": "SERIALNO",
-			"orderable": false,
-			"render": function(data) {
-				// 取消
-				return "<button type='button' class='btn btn-danger' onclick='cancelHoildayForm(1," + data + ")'><i class='fa fa-times' aria-hidden='true'></i></button>";
-				}
-            },
-            { "className": "dt-center", "targets": "_all" }
-        ],
-        ajax: {
-            url: 'ajax/update_ajax.php',
-            type: 'POST',
-            dataType: 'json',
-			data: {oper: "qry2"}
+        callback: function(result) {
+            if (!result) return;
+            jQuery.post(
+                "ajax/update_ajax.php", { oper: "cancel", flag: flag, sn: sn },
+                function(data) {
+                    toastr['success'](data);
+                    if (flag == 0)
+                        utb.ajax.reload();
+                    else
+                        ctb.ajax.reload();
+                }
+            );
         }
     });
 }
-
-function openHoildayForm(sn){
-	if(typeof sn == 'undefined')
-		return;
-	
-	var url = "update_form.php?sn=" + sn;
-	
-	$('.NewPage-IFrame').attr("src", url);
-	$('#fullscrModal').modal('show');
-}
-
-function cancelHoildayForm( flag, sn ){
-	bootbox.confirm({ 
-		message: "確定要取消該假單嗎？",
-		callback: function(result){
-			if( !result ) return;
-			jQuery.post( 
-				"ajax/update_ajax.php",
-				{oper: "cancel", flag: flag, sn: sn},
-				function( data ){
-					toastr['success'](data);
-					if( flag == 0 )
-						utb.ajax.reload();
-					else
-						ctb.ajax.reload();
-				}
-			);
-		}
-	});
-}
-
--- update.php
+++ update.php
@@ -5,75 +5,62 @@
     <div id="page-wrapper">
         <div class="container-fluid">
             <? include ("inc/page-header.php"); ?>
-            <div class="panel-group" id="accordion-1">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion-1" href="#collapse-One">
-                                未簽核完成 -- 修改/取消假單
-                            </a>
-                        </h4>
-                    </div>
-                    <div id="collapse-One" class="panel-collapse collapse in">
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-md-12 col-lg-12">
-                                    <table class="dt-Table table table-striped table-bordered" id="update_table" width="100%">
-                                        <thead>
-                                             <tr style="font-weight:bold">
-                                                <th>姓名</th>
-                                                <th>假別</th>
-                                                <th>起始日</th>
-                                                <th>終止日</th>
-                                                <th>起始時間</th>
-                                                <th>終止時間</th>
-                                                <th>總時數</th>
-                                                <th>職務代理人</th>
-                                                <th>功能</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody></tbody>
-                                    </table>
-                                </div>
-                            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><span class="clickable"><i class="glyphicon glyphicon-chevron-up"></i></span> 未簽核完成 -- 修改/取消假單</h3>
                         </div>
-                    </div>
-                </div>
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion-1" href="#collapse-Two">
-                                已簽核完成 -- 取消假單
-                            </a>
-                        </h4>
-                    </div>
-                    <div id="collapse-Two" class="panel-collapse collapse in">
                         <div class="panel-body">
-                            <div class="row">
-                                <div class="col-md-12 col-lg-12">
-                                    <table class="dt-Table table table-striped table-bordered" id="cancel_table" width="100%">
-                                        <thead>
-                                             <tr style="font-weight:bold">
-                                                <th>姓名</th>
-                                                <th>假別</th>
-                                                <th>起始日</th>
-                                                <th>終止日</th>
-                                                <th>起始時間</th>
-                                                <th>終止時間</th>
-                                                <th>總時數</th>
-                                                <th>職務代理人</th>
-                                                <th>功能</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody></tbody>
-                                    </table>
-                                </div>
-                            </div>
+                            <table class="dt-Table table table-striped table-bordered" id="update_table" width="100%">
+                                <thead>
+                                        <tr style="font-weight:bold">
+                                        <th>姓名</th>
+                                        <th>假別</th>
+                                        <th>起始日</th>
+                                        <th>終止日</th>
+                                        <th>起始時間</th>
+                                        <th>終止時間</th>
+                                        <th>總時數</th>
+                                        <th>職務代理人</th>
+                                        <th>功能</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><span class="clickable"><i class="glyphicon glyphicon-chevron-up"></i></span> 已簽核完成 -- 取消假單</h3>
+                        </div>
+                        <div class="panel-body">
+                            <table class="dt-Table table table-striped table-bordered" id="cancel_table" width="100%">
+                                <thead>
+                                        <tr style="font-weight:bold">
+                                        <th>姓名</th>
+                                        <th>假別</th>
+                                        <th>起始日</th>
+                                        <th>終止日</th>
+                                        <th>起始時間</th>
+                                        <th>終止時間</th>
+                                        <th>總時數</th>
+                                        <th>職務代理人</th>
+                                        <th>功能</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <!-- /.container-fluid -->
     </div>
