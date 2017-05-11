c963d29204a68b20b069f1daf64f52b05f292084
NejiYURI <halo010354@gmail.com>
2017-05-12 03:02:28 +0800

    Revert "差假統計報表"
    This reverts commit df86ebd98d56bd67cb3dc52f17169e56162ee05e.

--- js/all_holiday_query.js
+++ js/all_holiday_query.js
@@ -123,7 +123,6 @@ function CRUD(oper, id) {
     "scrollCollapse": true,
     "displayLength": 10,
     "paginate": true,
-    "destroy": true,
     "lengthChange": true,
     "processing": false,
     "serverSide": false,
@@ -136,93 +135,8 @@ function CRUD(oper, id) {
     "columns": [
         { "name": "DEPT_NAME" },
         { "name": "TITLE_NAME" },
-        { "name": "EMPL_NAME" },
-        { "name": "D1" },
-        { "name": "D2" },
-        { "name": "D3" },
-        { "name": "D4" },
-        { "name": "D5" },
-        { "name": "D6" },
-        { "name": "D7" },
-        { "name": "D8" },
-        { "name": "D9" },
-        { "name": "D10" },
-        { "name": "D11" },
-        { "name": "D12" },
-        { "name": "D13" },
-        { "name": "D14" },
-        { "name": "D15" },
-        { "name": "D16" },
-        { "name": "D17" }
+        { "name": "EMPL_NAME" }
     ]
 
 });
-
-/*<thead>
-<tr style="font-weight:bold">
-    <th>單位</th>
-    <th>職稱</th>
-    <th>姓名</th>
-    <th>出差</th>
-    <th>公假(02)</th>
-    <th>公假(03)</th>
-    <th>生理假</th>
-    <th>家庭照顧假</th>
-    <th>延長病假</th>
-    <th>事假</th>
-    <th>病假</th>
-    <th>休假(公)</th>
-    <th>婚假</th>
-    <th>婉假</th>
-    <th>喪假</th>
-    <th>加班補休</th>
-    <th>暑休</th>
-    <th>寒休</th>
-    <th>特休(勞)</th>
-    <th>其他</th>
-</tr>
-</thead>
-
-</table>*/
-
-/*$.ajax({
-    url: 'ajax/all_holiday_query_ajax.php',
-    data: {oper: 0, flag:flagval,tyearval:tyval,tmonthval:tmval,tdayval:tdval,syearval:syval,smonthval:smval,sdayval:sdval},
-    type: 'POST',
-    dataType: "json",
-    success: function(JData) {
-      $('#Btable').empty();
-      if (JData.error_code)
-          toastr["error"](JData.error_message);
-      else{
-
-        var row0 ="";
-        row0 = row0 + "<table  class='table table-striped table-bordered' width='100%'' cellspacing='0'><tbody><tr>";
-        row0 = row0 + "<td class='td1' style='text-align:center;'>單位</td>";
-        row0 = row0 + "<td class='td1' style='text-align:center;'>假別</td>";
-        row0 = row0 + "<td class='td1' style='text-align:center;'>職稱</td></tr>";
-
-
-        for(var i=0;i<JData.DEPT_NAME;i++){
-
-          row0 = row0 + "<tr><td  style='text-align:center;'>" ;
-          row0 = row0 + JData.DEPT_NAME[i];
-          row0 = row0 + "</td><td  style='text-align:center;'>" ;
-          row0 = row0 + JData.TITLE_NAME[i];
-          row0 = row0 + "</td><td  style='text-align:center;'>" ;
-          row0 = row0 + JData.EMPL_NAME[i];
-          row0 = row0 + "</td></tr>";
-
-
-
-
-        }
-        row0 = row0 + "</tbody></table>";
-        $('#Btable').append(row0);
-
-    }
-
-    },
-    error: function(xhr, ajaxOptions, thrownError) {console.log(xhr.responseText);alert(xhr.responseText);}
-});*/
 }
